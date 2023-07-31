dcaa5ac0f3c823b15ef2460867313b118a63b269
ortizfram <ortizfranco48@gmail.com>
2023-07-31 11:50:04 -0300

    starting miodels

--- workspace/models.py
+++ workspace/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class Element (models.Model):
+    title = models.CharField(max_length=100)
+    content =  models.CharField()
+    user = 
+    datetime = 
+    favourite =
