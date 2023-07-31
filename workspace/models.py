27501e6d13f10da7ce5f50de40fa70af73b8fa52
ortizfram <ortizfranco48@gmail.com>
2023-07-31 12:14:56 -0300

    init workspace model

--- workspace/models.py
+++ workspace/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Workspace (models.Model):
+    title = models.CharField(max_length=100)
+    logo = models.ImageField()
+    bookmarked = models.BooleanField(default=False, blank=True)
+    # settings = 
+        # To see element titles in admin ORM 
+    def __str__(self):
+        return self.title
+    
+class WorkspaceSetting (models.Model):
+    pass
+
 class Element (models.Model):
     title = models.CharField(max_length=100)
     content =  models.CharField()
@@ -10,4 +22,4 @@ class Element (models.Model):
 
         # To see element titles in admin ORM 
     def __str__(self):
-        return self.titl
+        return self.title
