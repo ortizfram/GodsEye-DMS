65d05777195ae1f389cf24acffe73d5daa2f3e7a
ortizfram <ortizfranco48@gmail.com>
2023-07-31 12:05:18 -0300

    element model

--- workspace/models.py
+++ workspace/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Element (models.Model):
     title = models.CharField(max_length=100)
     content =  models.CharField()
-    user = 
-    datetime = 
-    favourite =
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True) 
+    favourite = models.BooleanField(default=False, blank=True)
+
+        # To see element titles in admin ORM 
+    def __str__(self):
+        return self.titl
