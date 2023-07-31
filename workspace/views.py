bdd893c0ecf48d92148a02afa76a983b021c922b
ortizfram <ortizfranco48@gmail.com>
2023-07-31 13:45:38 -0300

    workspace model

--- workspace/views.py
+++ workspace/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Element
 
 def home(request):
     return render(request, "home.html")
 
 def workspace(request):
-    return render(request, "workspace.html")
+    # Fetch all elements from the database
+    elements = Element.objects.all()
+
+    # Check if there are any elements
+    if elements:
+        context = {'elements': elements}
+    else:
+        # Custom string when no elements exist
+        context = {'message': 'No elements yet.'}
+
+    # Pass context to the template
+    return render(request, "workspace.html", context)
